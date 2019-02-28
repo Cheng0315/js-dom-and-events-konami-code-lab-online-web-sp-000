37105bebd4121cf18204e6258e232b8f9ddd6143
Cheng Vue <csalvue@gmail.com>
2019-02-28 16:46:47 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
-
+function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', onKeyDownHandler);
   let index = 0;
@@ -21,3 +21,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
       index = 0;
     }
   }
+}
+
+init();
