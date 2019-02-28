8d302a9066a97088da04e25d36ef7a495afbd1b3
Cheng Vue <csalvue@gmail.com>
2019-02-28 16:18:57 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const main = document.body
-main.addEventListener(keydown, init(event));
+main.addEventListener('keydown', init(event));
 
 let index = 0;
 
