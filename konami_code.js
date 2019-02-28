cd960188e4f26d087129e3a8f4e3511853067d57
Cheng Vue <csalvue@gmail.com>
2019-02-28 16:26:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ let index = 0;
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-
+  console.log(e.which)
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
